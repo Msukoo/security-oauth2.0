65ad5f0362805c6cfbe90686f34ff19043102199
Msukoo <rnalsttnn@gmail.com>
2022-05-03 18:33:05 +0900

    first commit

---
+++ src/main/java/com/cos/security1/domain/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.cos.security1.domain.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index(){
+        return "index";
+    }
+}
